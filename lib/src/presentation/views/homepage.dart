4000cc0ccac067591aa8f16a9cfc25c96881226f
Quan Lee <quanxyzabc@gmail.comm>
2023-03-17 14:30:07 +0700

    Config project file

---
+++ lib/src/presentation/views/homepage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class HomePageView extends StatefulWidget {
+  const HomePageView({super.key});
+
+  @override
+  State<HomePageView> createState() => _HomePageViewState();
+}
+
+class _HomePageViewState extends State<HomePageView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
