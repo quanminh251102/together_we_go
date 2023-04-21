73fed9b63bbe9a9273485ab0a006a4190d0b7bcc
Nguyen Hoang Kiet <skill1sp2@gmail.com>
2023-04-21 19:28:34 +0700

    Login Facebook Call Api

--- lib/src/presentation/cubits/signin/signin_cubit.dart
+++ lib/src/presentation/cubits/signin/signin_cubit.dart
@@ -5,8 +5,10 @@ import 'package:equatable/equatable.dart';
 import 'package:http/http.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter_login_facebook/flutter_login_facebook.dart';
+import 'package:path_provider/path_provider.dart';
 import '../../../config/url/config.dart';
 import '../app_user.dart';
+import '../chat/chat_rooms_cubit.dart';
 
 part 'signin_state.dart';
 
@@ -15,6 +17,7 @@ class SigninCubit extends Cubit<SigninState> {
   final GoogleSignIn _googleSignIn = GoogleSignIn();
   bool _isLoggedIn = false;
   late GoogleSignInAccount _userObj;
+  late FacebookLogin pluginFacebook = FacebookLogin();
 
   void SignIn(
     String email,
@@ -92,13 +95,14 @@ class SigninCubit extends Cubit<SigninState> {
   void SignInWithFacebook() async {
     try {
       // Create an instance of FacebookLogin
-      final fb = FacebookLogin();
-
+      //this.pluginFacebook = new FacebookLogin();
+      await this.pluginFacebook.logOut();
 // Log in
-      final res = await fb.logIn(permissions: [
+      final res = await this.pluginFacebook.logIn(permissions: [
         FacebookPermission.publicProfile,
         FacebookPermission.email,
       ]);
+      emit(SigninLoading());
 
 // Check result status
       switch (res.status) {
@@ -107,28 +111,52 @@ class SigninCubit extends Cubit<SigninState> {
 
           // Send access token to server for validation and auth
           final FacebookAccessToken accessToken = res.accessToken!;
+          print('date ${accessToken.expires}');
           print('Access token: ${accessToken.token}');
 
           // Get profile data
-          final profile = await fb.getUserProfile();
+          final profile = await this.pluginFacebook.getUserProfile();
           print('Hello, ${profile?.name}! You ID: ${profile?.userId}');
 
           // Get user profile image url
-          final imageUrl = await fb.getProfileImageUrl(width: 100);
+          final imageUrl =
+              await this.pluginFacebook.getProfileImageUrl(width: 100);
           print('Your profile image: $imageUrl');
 
           // Get email (since we request email permission)
-          final email = await fb.getUserEmail();
+          final email = await this.pluginFacebook.getUserEmail();
           // But user can decline permission
           if (email != null) print('And your email is $email');
 
+          Response response =
+              await post(Uri.parse('$baseUrl/api/auth/loginWithGoogle'), body: {
+            'email': '${profile?.userId}',
+            'name': profile?.name,
+            'avatar': imageUrl,
+          });
+          print('Successfully Login Facebook');
+          print(response.body);
+          if (response.statusCode == 200) {
+            final new_t = response.body;
+            final data = json.decode(new_t) as Map<String, dynamic>;
+
+            appUser.init(data["user_id"], data["user_name"], data["user_email"],
+                data["user_avatar"]);
+
+            emit(SigninSuccess(''));
+          } else {
+            print("Failed");
+            emit(SigninError('Đã xảy ra lổi phía server'));
+          }
           break;
         case FacebookLoginStatus.cancel:
           // User cancel log in
+          emit(SigninError('Đã xảy ra lổi khi xác thực facebook'));
           break;
         case FacebookLoginStatus.error:
           // Log in failed
           print('Error while log in: ${res.error}');
+          emit(SigninError('Đã xảy ra lổi khi xác thực facebook'));
           break;
       }
     } catch (e) {
@@ -137,8 +165,16 @@ class SigninCubit extends Cubit<SigninState> {
   }
 
   void Logout() async {
+    emit(SigninLoading());
     _googleSignIn.signOut().then((value) {
       _isLoggedIn = false;
     }).catchError((e) {});
+
+    await this.pluginFacebook.logOut();
+
+    print(
+        'logout ${await this.pluginFacebook.getUserEmail()} ${await this.pluginFacebook.isLoggedIn}');
+
+    emit(SigninInitial());
   }
 }
